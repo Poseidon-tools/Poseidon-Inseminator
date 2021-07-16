8a2ba73aff334e5aedd82b48759a3d96d2eec7d5
Muciojad <muciojad@gmail.com>
2021-07-16 14:16:32 +0200

    created ScriptableObjectDependencyResolver

---
+++ Assets/Scripts/SDI/DI/DependencyResolvers/ScriptableObject/ScriptableObjectDependencyResolver.cs
@@ -0,0 +1,51 @@
+﻿namespace SDI.DI.DependencyResolvers.ScriptableObject
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using Installers;
+    using Resolver;
+    using Sirenix.OdinInspector;
+    using UnityEngine;
+    using Utils;
+
+    public class ScriptableObjectDependencyResolver : DependencyResolver
+    {
+        #region Inspector
+        [SerializeField, BoxGroup("Declared Installers"), InfoBox("Remember to drag your installer to this list!", InfoMessageType.Warning)]
+        [HideLabel]
+        private List<Installer> declaredInstallers = new List<Installer>();
+        [SerializeField, BoxGroup("ScriptableObjects"), InfoBox("Hit refresh button to refresh values.", InfoMessageType.Info)]
+        [HideLabel]
+        private List<ScriptableObject> scriptableObjects = new List<ScriptableObject>();
+        #endregion
+
+        #region Resolving
+        private void ResolveScriptableObjects()
+        {
+            foreach (var scriptableObject in scriptableObjects)
+            {
+                var instance = (object)scriptableObject;
+                ResolveDependencies(ref instance);
+            }
+        }
+        #endregion
+        
+        #region Unity Methods
+        private void Awake()
+        {
+            Install(declaredInstallers);
+            ResolveScriptableObjects();
+        }
+        #endregion
+
+        #region Editor
+#if UNITY_EDITOR
+        [BoxGroup("ScriptableObjects"), Button(ButtonSizes.Large)]
+        private void RefreshScriptableObjects()
+        {
+            scriptableObjects = AssetsUtils.GetAllInstances<ScriptableObject>().ToList();
+        }
+#endif
+        #endregion
+    }
+}
---
+++ Assets/Scripts/SDI/DI/Example/ExampleSOInjection.cs
@@ -0,0 +1,13 @@
+﻿namespace SDI.DI.Example
+{
+    using UnityEngine;
+
+    [CreateAssetMenu(menuName = "DI/Example/ExampleSOInjection")]
+    public class ExampleSOInjection : ScriptableObject
+    {
+        #region Inspector
+        [SerializeField, Attributes.Injectable]
+        private MessageData messageData;
+        #endregion
+    }
+}
